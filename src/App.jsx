15f85ef9bc007d3e6b2a8c6e20ddde04e436567c
alkozp <myredirect@ukr.net>
2024-03-25 00:43:48 +0200

    app updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // TODO TODOReducer App
-import { useReducer } from "react";
+import { useReducer, Fragment } from "react";
 import "./App.css";
 
 const initTodoList = [
-  { id: 1, name: "Learn React", state: true },
-  { id: 2, name: "Learn Redux", state: true },
-  { id: 3, name: "Learn JS", state: true },
-  { id: 4, name: "Learn HTML", state: false },
-  { id: 5, name: "Learn CSS", state: false },
-  { id: 6, name: "Learn SASS", state: true },
-  { id: 7, name: "Learn Tailwind", state: true },
-  { id: 8, name: "Learn Bootstrap", state: true },
+  { id: 1, name: "Learn React", complete: false },
+  { id: 2, name: "Learn Redux", complete: false },
+  { id: 3, name: "Learn JS", complete: false },
+  { id: 4, name: "Learn HTML", complete: true },
+  { id: 5, name: "Learn CSS", complete: true },
+  { id: 6, name: "Learn SASS", complete: false },
+  { id: 7, name: "Learn Tailwind", complete: false },
+  { id: 8, name: "Learn Bootstrap", complete: false },
 ];
 
 const TODO_ACTION = {
@@ -25,7 +25,7 @@ function todoReducer(todoList, action) {
     case TODO_ACTION.DONE:
       return todoList.map((todo) => {
         if (todo.id === action.id) {
-          return { ...todo, state: !todo.state };
+          return { ...todo, complete: !todo.complete };
         }
         return todo;
       });
@@ -46,32 +46,34 @@ function App() {
       <div>
         <h1>Todo List</h1>
         <input type="text" name="todo" id="todo" placeholder="New todo" />
-        <button onClick={() => dispatch({ type: TODO_ACTION.NEW })}>Add</button>
+        <button
+          onClick={() => dispatch({ type: TODO_ACTION.NEW, name: todo.value })}
+        >
+          Add
+        </button>
       </div>
       <hr />
       <div className="todo-list">
         {todoList.map((todo) => {
           return (
-            <>
+            <Fragment key={todo.id}>
               <span
-                key={todo.id}
                 {...{
                   style: {
-                    textDecoration: !todo.state ? "line-through" : "none",
+                    textDecoration: todo.complete ? "line-through" : "none",
                   },
                 }}
               >
                 {todo.name}
               </span>
               <button
-                key={todo.id + 1000}
                 onClick={() =>
                   dispatch({ type: TODO_ACTION.DONE, id: todo.id })
                 }
               >
                 Done
               </button>
-            </>
+            </Fragment>
           );
         })}
       </div>
